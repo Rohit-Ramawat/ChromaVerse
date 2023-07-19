3167e26cc5be789d346a9b0a01df01c1fed52d01
shivam sahu <sshubham026@gmail.com>
2023-07-19 23:42:46 +0530

    my work

---
+++ Online_Shopping_Application_Backend/src/main/java/com/masai/controller/CustomerController.java
@@ -0,0 +1,68 @@
+package com.masai.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.masai.model.Customer;
+import com.masai.service.CustomerService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    private CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addCustomer(@RequestBody Customer customer) {
+        customerService.addCustomer(customer);
+        return new ResponseEntity<>("Customer added successfully.", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity<String> updateCustomer(@PathVariable Integer customerId, @RequestBody Customer customer) {
+        customer.setCustomerId(customerId);
+        customerService.updateCustomer(customer);
+        return new ResponseEntity<>("Customer updated successfully.", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{customerId}")
+    public ResponseEntity<String> removeCustomer(@PathVariable Integer customerId) {
+        customerService.removeCustomer(customerId);
+        return new ResponseEntity<>("Customer removed successfully.", HttpStatus.OK);
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Customer> viewCustomer(@PathVariable Integer customerId) {
+        Optional<Customer> customer = customerService.viewCustomer(customerId);
+        return customer.map(c -> new ResponseEntity<>(c, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> viewAllCustomers() {
+        List<Customer> customers = customerService.viewAllCustomers();
+        if (!customers.isEmpty()) {
+            return new ResponseEntity<>(customers, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+}
+
--- Online_Shopping_Application_Backend/src/main/java/com/masai/model/Order.java
+++ Online_Shopping_Application_Backend/src/main/java/com/masai/model/Order.java
@@ -2,11 +2,16 @@ package com.masai.model;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
 
@@ -16,7 +21,7 @@ public class Order {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long orderId;
+	private Integer orderId;
 	
 	@PastOrPresent
 	private LocalDate orderDate;
@@ -24,129 +29,12 @@ public class Order {
 	@NotBlank(message = "please provide valid orderstatus") 
 	private String orderStatus;
 	
-	@ElementCollection
-    @CollectionTable(name = "order_product_mapping", joinColumns = @JoinColumn(name = "order_id"))
-    @MapKeyJoinColumn(name = "product_id")
-    @Column(name = "quantity")
-    private Map<Product, Integer> productList = new HashMap<>();
-
-    @ManyToOne
-    @JoinColumn(name = "customer_id")
-    private Customer customer;
-
-    @ManyToOne
-    @JoinColumn(name = "address_id")
-    private Address address;
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
+	private Customer customer;
 	
-
 	@OneToMany(fetch = FetchType.EAGER)
 	private List<Product> products = new ArrayList<>();
-
-	public Order(@PastOrPresent LocalDate orderDate,
-			@NotBlank(message = "please provide valid orderstatus") String orderStatus) {
-		super();
-		this.orderDate = orderDate;
-		this.orderStatus = orderStatus;
-	}
-
-	public Order() {
-        // Default constructor
-    }
-
-    public Order(Long orderId, LocalDate orderDate, Customer customer, Address address) {
-        this.orderId = orderId;
-        this.orderDate = orderDate;
-        this.customer = customer;
-        this.address = address;
-    }
-
-    // Getters and Setters
-    public Long getOrderId() {
-        return orderId;
-    }
-
-
-    public LocalDate getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(LocalDate orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public String getOrderStatus() {
-        return orderStatus;
-    }
-
-    public void setOrderStatus(String orderStatus) {
-        this.orderStatus = orderStatus;
-    }
-
-    public Map<Product, Integer> getProductList() {
-        return productList;
-    }
-
-    public void setProductList(Map<Product, Integer> productList) {
-        this.productList = productList;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public void addProduct(Product product, int quantity) {
-        if (quantity <= 0) {
-            throw new IllegalArgumentException("Quantity must be greater than zero.");
-        }
-
-        // Check if the product is already in the order
-        int existingQuantity = productList.getOrDefault(product, 0);
-
-        // Check if the requested quantity is available in stock
-        int availableQuantity = product.getQuantity();
-        if (existingQuantity + quantity > availableQuantity) {
-            throw new IllegalArgumentException("Requested quantity exceeds available stock.");
-        }
-
-        // Update the product's quantity in the order and deduct from the product's total quantity
-        productList.put(product, existingQuantity + quantity);
-        product.setQuantity(availableQuantity - quantity);
-    }
-
-    // Method to remove a certain quantity of a product from the order
-    // If the quantity becomes zero, remove the product from the order
-    public void removeProduct(Product product, int quantity) {
-        if (quantity <= 0) {
-            throw new IllegalArgumentException("Quantity must be greater than zero.");
-        }
-
-        // Check if the product is in the order
-        int existingQuantity = productList.getOrDefault(product, 0);
-        if (existingQuantity == 0) {
-            throw new IllegalArgumentException("Product not found in the order.");
-        }
-
-        // Update the product's quantity in the order and add back to the product's total quantity
-        if (existingQuantity - quantity <= 0) {
-            productList.remove(product);
-        } else {
-            productList.put(product, existingQuantity - quantity);
-        }
-        product.setQuantity(product.getQuantity() + quantity);
-    }
-
 	
 	
 }
