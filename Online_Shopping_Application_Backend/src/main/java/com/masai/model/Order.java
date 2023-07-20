893121a62f8905965f41ab7b0ec620e1bfc6b7e5
Ravi Sharma <sraa7877@gmail.com>
2023-07-20 17:07:22 +0530

    re-add

--- Online_Shopping_Application_Backend/src/main/java/com/masai/model/Order.java
+++ Online_Shopping_Application_Backend/src/main/java/com/masai/model/Order.java
@@ -2,7 +2,10 @@ package com.masai.model;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -18,23 +21,102 @@ import jakarta.validation.constraints.PastOrPresent;
 @Entity
 @Table(name = "OrderTable")
 public class Order {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer orderId;
-	
-	@PastOrPresent
-	private LocalDate orderDate;
-	
-	@NotBlank(message = "please provide valid orderstatus") 
-	private String orderStatus;
-	
-	@ManyToOne
-	@JoinColumn(name = "customer_id")
-	private Customer customer;
-	
-	@OneToMany(fetch = FetchType.EAGER)
-	private List<Product> products = new ArrayList<>();
-	
-	
+    private Long orderId;
+    private LocalDate orderDate;
+    private String orderStatus = "pending";
+    private Map<Product, Integer> productList = new HashMap<>();
+    private Customer customer;
+    private Address address;
+
+    // Constructors
+    public Order() {
+        // Default constructor
+    }
+
+    public Order(Long orderId, LocalDate orderDate, Customer customer, Address address) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.customer = customer;
+        this.address = address;
+    }
+
+    // Getters and Setters
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Map<Product, Integer> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(Map<Product, Integer> productList) {
+        this.productList = productList;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    
+    public void addProduct(Product product, int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity must be greater than zero.");
+        }
+        productList.put(product, productList.getOrDefault(product, 0) + quantity);
+    }
+
+    public void removeProduct(Product product, int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity must be greater than zero.");
+        }
+        int currentQuantity = productList.getOrDefault(product, 0);
+        int newQuantity = currentQuantity - quantity;
+        if (newQuantity <= 0) {
+            productList.remove(product);
+        } else {
+            productList.put(product, newQuantity);
+        }
+    }
+
+    public double getTotalPrice() {
+        double totalPrice = 0;
+        for (Map.Entry<Product, Integer> entry : productList.entrySet()) {
+            Product product = entry.getKey();
+            int quantity = entry.getValue();
+            totalPrice += product.getPrice() * quantity;
+        }
+        return totalPrice;
+    }
 }
+
